8cdf58f50e3700672a76141650d24c27d94fe919
kihisee <631680828@qq.com>
2018-06-06 22:51:09 +0800

    elm提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+
+  <header></header>
+  <div class="tab">
+    I am  tab
+  </div>
+  <div class="content">
+    I am 
+  </div>
+  
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+import header from './components/hearder/header.vue'
+export default {
+  components:{
+    header,
+  }
+}
+</script>
+
---
+++ src/components/hearder/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        I am header
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
